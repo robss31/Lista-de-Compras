f0577cda1555840ac43ceb735e9afb6832600cbe
robss31 <robsonss231@gmail.com>
2023-08-02 20:39:30 -0300

    Atualização da Lista de Compras jsx/css

--- src/ListaComp.jsx
+++ src/ListaComp.jsx
@@ -3,22 +3,17 @@ import React, { useState, useEffect, useRef } from "react";
 import './ListaComp.css';
 
 
-
-
 function ListaComp() {
 
     const listaStorage = localStorage.getItem('Lista');
 
-
     const [lista, setLista] = useState(listaStorage ? JSON.parse(listaStorage) : []);
     const [name, setName] = useState('');
-    const [venci, setVenci] = useState('');
+    const [quantidade, setQuantidade] = useState('');
     const [valor, setValor] = useState('');
     const [spanValue, setSpanValue] = useState(0);
     const [result, setResult] = useState(0);
     const spanRef = useRef(null);
-
-
 
 
     useEffect(() => {
@@ -29,24 +24,22 @@ function ListaComp() {
     function adicionaItem(e) {
         e.preventDefault();
 
-        if (name === '' || venci === '' || valor === '') {
+        if (name === '' || quantidade === '' || valor === '') {
             alert("Preencha todos os campos");
             return;
         }
 
-        setLista([...lista, { text: name, data: venci, custo: valor,  isCompleted: false }])
+        setLista([...lista, { text: name, qtde: quantidade, custo: valor, saldo: result, isCompleted: false }])
         setName("");
-        setVenci("");
+        setQuantidade("");
         setValor("");
-       
+        setsaldo("");
+
         document.getElementById('inputconta');
         document.getElementById('inputconta1');
         document.getElementById('inputconta2');
-     
     }
 
-
-  /*
     //Captura o valor inicial da <span> após a renderização
     useEffect(() => {
         const initialSpanValue = parseInt(spanRef.current.innerText, 0);
@@ -54,26 +47,16 @@ function ListaComp() {
     }, []);
 
     // Função para somar o valor com o input e atualizar o resultado
-  const handleSum = () => {
-        const parsedInputConta2 = parseInt(inputConta2, 10);
+    const handleSum = () => {
 
-        if (!isNaN(parsedInputConta2)) {
-            setResult(parsedInputConta - valor);
-            setInputValue(parsedInputValue - valor);
-            // setInputValue('');
+        const parsedValor = parseFloat(valor, 10);
+        const parsedQuantidade = parseFloat(quantidade, 10);
 
-
-
-        } else{
-            //setInputValue(setResult);
-            //setResult(inputValue - valor)
-
-
-        }
-
+        const valTotal = (parsedValor * parsedQuantidade);
+        setResult(valTotal + result);
+        setInputConta2(valor += valor);
 
     };
-*/
 
     function clicou(index) {
         const listaAux = [...lista];
@@ -83,7 +66,7 @@ function ListaComp() {
 
     function deleta(index) {
         const listaAux = [...lista];
-        listaAux.splice(index, 1);
+        (listaAux.splice(index, 1));
         setLista(listaAux);
     }
 
@@ -91,11 +74,9 @@ function ListaComp() {
         setLista([]);
     }
 
-
-
     return (
         // restante do código permanece inalterado...
-        <div className="container2">
+        <div className="container">
 
             <h1>Lista de Compras</h1>
 
@@ -104,17 +85,16 @@ function ListaComp() {
 
 
                 <input id="inputconta" type="text" value={name} onChange={(e) => { setName(e.target.value) }} placeholder="  Nome do Produto" />
-                <input id="inputconta1" type="text" value={venci} onChange={(e) => { setVenci(e.target.value) }} placeholder=" Quantidade" />
-                <input id="inputconta2" type="number" value={valor} onChange={(e) => { setValor(e.target.value) }} placeholder="  Valor" />
+                <input id="inputconta1" type="number" value={quantidade} onChange={(e) => { setQuantidade(e.target.value) }} placeholder=" Quantidade" />
+                <input id="inputconta2" type="number" value={valor} onChange={(e) => { setValor(e.target.value) }} placeholder="  Valor Unitário " />
 
-                <button type="submit" id="btn1">add</button>
+                <button type="submit" id="btn1" onClick={handleSum}>add</button>
 
+                <span ref={spanRef}></span>
 
-
-               
             </form>
 
-            <div className="listaContas">
+            <div className="listaCompra">
 
                 {
                     lista.length < 1
@@ -124,16 +104,14 @@ function ListaComp() {
                         :
                         lista.map((item, index) => (
 
-
                             <div
                                 key={index}
                                 className={item.isCompleted ? "item completo " : "item "}
                             >
                                 <span onClick={() => { clicou(index) }}>{item.text}</span>
-                                <span onClick={() => { clicou(index) }}>{item.data}</span>
+                                <span onClick={() => { clicou(index) }}>{item.qtde}</span>
                                 <span onClick={() => { clicou(index) }}>{item.custo}</span>
-                                
-
+                                <span onClick={() => { clicou(index) }}>Total: {item.saldo}</span>
 
 
                                 <button onClick={() => { deleta(index) }} className="deletar">deletar</button>
@@ -144,10 +122,7 @@ function ListaComp() {
 
                 }
 
-
             </div>
-
-
 
             {
                 lista.length > 0 &&
